36c5a9780e47585a1dbeac5e6692185dbece9347
Milad Ghadami Yazdi <milad.ghadami@appliedtechnology.se>
2020-02-01 17:32:51 +0100

    Retrieve user inputs

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ function Autobind (_1: any, _2: string, descriptor: PropertyDescriptor) {
    return adjustedMethod;
 }
 
-
 class ProjectInput {
   templteElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -51,10 +50,29 @@ class ProjectInput {
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
+  private inputsPayload (): [string, string, number]{
+      const titleInput = this.titleInputElement.value
+      const descInput = this.descriptionInputElement.value
+      const peopleInput = this.peopleInputElement.value
+      if (titleInput.length === 0 || descInput.length === 0 || peopleInput.length === 0) {
+         throw new Error("Title or desc or people input is wrong")
+      } 
+      return [titleInput, descInput, +peopleInput]
+  }
+  clearInputs(){
+    this.titleInputElement.value = ""
+    this.descriptionInputElement.value = ""
+    this.peopleInputElement.value = ""
+  }
   @Autobind
   private handleSubmit(e: Event) {
     e.preventDefault();
-    console.log(this.titleInputElement.value);
+   const userInput = this.inputsPayload()
+   if (Array.isArray(userInput)){
+       const [title, desc, people] = userInput;
+       console.log(title, desc, people)
+   }
+   this.clearInputs();
   }
   private InputText() {
     this.element.addEventListener("submit", this.handleSubmit);
